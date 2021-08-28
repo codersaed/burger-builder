0d84fa59ad1a33085b516de823fce8dc79e3a924
codersaed <codersaed@gmail.com>
2021-08-28 18:28:00 +0600

    nav

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Header.css";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+import Logo from "../../assets/logo.png";
+
+const Header = () => {
+  return (
+    <div
+      className="Navigation"
+      style={{
+        backgroundColor: "#D70F64",
+        height: "70px",
+      }}
+    >
+      <Navbar className="container">
+        <NavbarBrand href="/" className="ml-md-5 Brand">
+          <img src={Logo} alt="Logo" width="80px" />
+        </NavbarBrand>
+        <Nav className="mr-md-5">
+          <NavItem>
+            <NavLink href="#" className="NavLink">
+              Something
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
